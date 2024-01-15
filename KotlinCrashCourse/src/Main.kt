4b83875ce739a4df18f5700db1f46037cdafb831
sushil <baymax732001@gmail.com>
2024-01-15 22:34:08 +0530

    learned var,val,String interpolation , println and print

--- KotlinCrashCourse/src/Main.kt
+++ KotlinCrashCourse/src/Main.kt
@@ -1,3 +1,17 @@
 fun main() {
+    var name = "sushil"
+    val userId = 2312312
+    val k = "sushil is fixed"
     println("Hello World!")
+    printName()
+    println(name);
+    println(k);
+    println(userId);
+    println("String interpolatiion is :"+name);
+    println("String interpolation without + is ${name}")
+    println("String interpolation without + with multiple value is ${name+k}")
+
+}
+fun printName(){
+    println("Print my name")
 }
