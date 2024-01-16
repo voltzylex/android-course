8f1e0a8c112f89b3e14712f5a6f28104db481892
sushil <baymax732001@gmail.com>
2024-01-16 23:03:16 +0530

    learned type interference , data types, fix data types

--- KotlinCrashCourse/src/Main.kt
+++ KotlinCrashCourse/src/Main.kt
@@ -2,6 +2,7 @@ fun main() {
     var name = "sushil"
     val userId = 2312312
     val k = "sushil is fixed"
+    var number:Int = 60
     println("Hello World!")
     printName()
     println(name);
@@ -10,6 +11,10 @@ fun main() {
     println("String interpolatiion is :"+name);
     println("String interpolation without + is ${name}")
     println("String interpolation without + with multiple value is ${name+k}")
+    println("This is a number $number")
+    val arr:Array<String> = arrayOf("first","second","third")
+    println("This is a array it can be declared using arrayof ${arr[1]}")
+
 
 }
 fun printName(){
